77ebe40836b11aa24705becec2941f7cdf369282
actioninja <actioninja@criticalaction.net>
2023-06-01 19:21:39 -0700

    lots of changes due to formatting pass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import {remarkReadingTime} from "./remark/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      server: {
-          watch: {
-              ignored: ['**/.idea/**']
-          }
-      },
+    server: {
+      watch: {
+        ignored: ['**/.idea/**']
+      }
+    },
     optimizeDeps: {
       exclude: ['.idea/']
     }
   },
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
-import { z, defineCollection } from "astro:content";
+import {z, defineCollection} from "astro:content";
 
 const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        category: z.string(),
-        date: z.date(),
-        draft: z.boolean(),
-        tags: z.array(z.string()),
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    category: z.string(),
+    date: z.date(),
+    draft: z.boolean(),
+    tags: z.array(z.string()).optional(),
+  })
 })
 
 export const collections = {
-    'blog': blogCollection,
+  'blog': blogCollection,
 }
